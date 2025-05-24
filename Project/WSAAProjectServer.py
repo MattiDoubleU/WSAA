acd2ed0dd4ebf8dd75ee49aaa3e9ce4413466233
MattiDoubleU <luciernagas@gmx.de>
2025-05-24 17:59:39 +0100

    Adding simple default route to flask server

--- Project/WSAAProjectServer.py
+++ Project/WSAAProjectServer.py
@@ -1,9 +1,15 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from flask_cors import cross_origin
 from ExpenseDAO import expenseDAO  
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes.
+
+@app.route('/')
+@cross_origin()
+def index():
+    return "Hello, World!"
 
 # Get all expenses.
 @app.route('/api/expenses', methods=['GET'])
